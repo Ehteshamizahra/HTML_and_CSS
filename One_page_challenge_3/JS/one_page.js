41e32f6253cf8f919e7c5c619e4085ec416541f4
Zahra Ehteshami <ehteshamizahra@gmail.com>
2022-05-04 17:00:24 +0100

    Added a new functionality

---
+++ One_page_challenge_3/JS/one_page.js
@@ -0,0 +1,39 @@
+function openModal() {
+    document.getElementById("myModal").style.display = "block";
+    console.log("open");
+  }
+  
+  function closeModal() {
+    document.getElementById("myModal").style.display = "none";
+    console.log("close");
+  }
+  
+  var slideIndex = 1;
+  showSlides(slideIndex);
+  
+  function plusSlides(n) {
+    showSlides(slideIndex += n);
+  }
+  
+  function currentSlide(n) {
+    showSlides(slideIndex = n);
+  }
+  
+  function showSlides(n) {
+    console.log("func");
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("demo");
+    var captionText = document.getElementById("caption");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+    captionText.innerHTML = dots[slideIndex-1].alt;
+  }
